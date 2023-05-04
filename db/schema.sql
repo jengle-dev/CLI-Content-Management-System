f6cfe9fadcb296f9644384dcd3e1a66bc773071a
jengle-dev <j.engle.dev@gmail.com>
2023-05-04 14:17:45 -0500

    updated syntax issue and messed up field name for role. seeds now works

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS human_resources_db;
 CREATE DATABASE human_resources_db;
-
 USE human_resources_db;
 
 CREATE TABLE department (
@@ -9,25 +8,29 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
+    CONSTRAINT FK_department_id
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
+    manager_id INT DEFAULT NULL,
+    CONSTRAINT FK_role_id
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
     ON DELETE SET NULL
 );
+
+ALTER TABLE employee
+    ADD FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL;
